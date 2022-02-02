36767ae76636229ba3961fd21dbcaf1c85b4fd15
evitla <s.dinmuhamed@gmail.com>
2022-02-02 11:48:47 +0600

    feat: add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
 
-const App = () => <h1>RS Lang</h1>;
+import GlobalStyles from './styles/global';
+import { baseTheme } from './styles/theme';
+
+const App = () => (
+  <ThemeProvider theme={baseTheme}>
+    <h1>RS Lang</h1>
+    <GlobalStyles />
+  </ThemeProvider>
+);
 
 export default App;
