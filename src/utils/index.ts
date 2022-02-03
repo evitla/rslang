73b59c36f8f0c1f3cac3001551f9fb81b0afe3c4
evitla <s.dinmuhamed@gmail.com>
2022-02-03 20:07:51 +0600

    feat: add playAudio function

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,3 +3,17 @@ export const getAll = async <T>(url: string, queryParams = ''): Promise<T> => {
   const data = await response.json();
   return data;
 };
+
+export const playAudio = (src: string) => {
+  return new Promise((res) => {
+    const audio = new Audio();
+    audio.src = src;
+    audio.currentTime = 0;
+    audio.play();
+
+    audio.onended = () => {
+      audio.currentTime = 0;
+      res('ended');
+    };
+  });
+};
