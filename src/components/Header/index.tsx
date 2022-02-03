5ee04b576c45aa5aca87cae9fd022efc7fe8ddbd
evitla <s.dinmuhamed@gmail.com>
2022-02-03 11:44:56 +0600

    feat: add StyledHeader

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,23 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { StyledHeader } from './style';
+import { FlexWrapper } from '../../styles/wrapper';
+
 const Header = () => (
-  <header>
-    <Link to="/">RS Lang Logo</Link>
-    <nav>
-      <ul>
-        <li>
-          <Link to="book">Электронный учебник</Link>
-        </li>
-        <li>
-          <Link to="games">Мини-игры</Link>
-        </li>
-        <li>
-          <Link to="user">User Icon</Link>
-        </li>
-      </ul>
-    </nav>
-  </header>
+  <StyledHeader>
+    <FlexWrapper>
+      <Link to="/">RS Lang Logo</Link>
+      <nav>
+        <ul>
+          <li>
+            <Link to="book">Электронный учебник</Link>
+          </li>
+          <li>
+            <Link to="games">Мини-игры</Link>
+          </li>
+          <li>
+            <Link to="user">User Icon</Link>
+          </li>
+        </ul>
+      </nav>
+    </FlexWrapper>
+  </StyledHeader>
 );
 
 export default Header;
