d001ed73c1a5242773ffe8d4710fa9cd97cf5b02
evitla <s.dinmuhamed@gmail.com>
2022-02-03 11:14:12 +0600

    feat: add Header, Footer, page routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
+import { Route, Routes } from 'react-router';
 import { ThemeProvider } from 'styled-components';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import { Audiocall, Book, Games, Home, Sprint, User } from './pages';
 
 import GlobalStyles from './styles/global';
 import { baseTheme } from './styles/theme';
 
 const App = () => (
   <ThemeProvider theme={baseTheme}>
-    <h1>RS Lang</h1>
+    <Header />
+    <hr />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="book" element={<Book />} />
+      <Route path="user" element={<User />} />
+      <Route path="games" element={<Games />}>
+        <Route path="audiocall" element={<Audiocall />} />
+        <Route path="sprint" element={<Sprint />} />
+      </Route>
+    </Routes>
+    <hr />
+    <Footer />
     <GlobalStyles />
   </ThemeProvider>
 );
