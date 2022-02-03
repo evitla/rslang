7bcbe53c1fca2929657c5175abd7ade2a6be527b
evitla <s.dinmuhamed@gmail.com>
2022-02-03 11:45:07 +0600

    feat: add StyledFooter

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,48 +1,53 @@
 import React from 'react';
 import { useLocation } from 'react-router';
 
+import { StyledFooter } from './style';
+import { Wrapper } from '../../styles/wrapper';
+
 const Footer = () => {
   const { pathname } = useLocation();
 
   return (
     <>
       {!pathname.includes('games') && (
-        <footer>
-          <ul>
-            <li>
-              <ul>
-                <li>
-                  <a
-                    href="https://github.com/evitla"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Dinmukhamed Sailaubek
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://github.com/hxnxikazuchi"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Ruslan Kupzhassarov
-                  </a>
-                </li>
-              </ul>
-            </li>
-            <li>2022</li>
-            <li>
-              <a
-                href="https://rs.school/js/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                RS School
-              </a>
-            </li>
-          </ul>
-        </footer>
+        <StyledFooter>
+          <Wrapper>
+            <ul>
+              <li>
+                <ul>
+                  <li>
+                    <a
+                      href="https://github.com/evitla"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Dinmukhamed Sailaubek
+                    </a>
+                  </li>
+                  <li>
+                    <a
+                      href="https://github.com/hxnxikazuchi"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Ruslan Kupzhassarov
+                    </a>
+                  </li>
+                </ul>
+              </li>
+              <li>2022</li>
+              <li>
+                <a
+                  href="https://rs.school/js/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  RS School
+                </a>
+              </li>
+            </ul>
+          </Wrapper>
+        </StyledFooter>
       )}
     </>
   );
---
+++ src/components/Footer/style.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const StyledFooter = styled.footer`
+  ul {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    ul {
+      width: 350px;
+    }
+  }
+`;
