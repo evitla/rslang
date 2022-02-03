7d46d3a8f34f9dc7430ccd5a68130f64a2456d69
evitla <s.dinmuhamed@gmail.com>
2022-02-03 18:27:43 +0600

    feat: render words

--- src/pages/Book/index.tsx
+++ src/pages/Book/index.tsx
@@ -1,5 +1,25 @@
 import React from 'react';
+import useFetchWords from '../../hooks/useFetchWords';
 
-const Book = () => <h2>Book page</h2>;
+const Book = () => {
+  const { words, isLoading, isError } = useFetchWords(0, 0);
+
+  return (
+    <>
+      <h2>Book page</h2>
+      {isLoading ? (
+        <div>Fetching words...</div>
+      ) : isError ? (
+        <div>Error while fetching</div>
+      ) : (
+        <ol>
+          {words.map((word) => (
+            <li key={word.id}>{word.word}</li>
+          ))}
+        </ol>
+      )}
+    </>
+  );
+};
 
 export default Book;
