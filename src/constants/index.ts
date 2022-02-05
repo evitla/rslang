4b5466d7af811911cf0d90eae1bf91083d66304f
evitla <s.dinmuhamed@gmail.com>
2022-02-05 23:32:09 +0600

    refactor: add minimum password length constant

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -15,3 +15,4 @@ export const SIGNIN_URL = `${DB_URL}/signin`;
 export const NAME_VALIDATION =
   /^[^(0-9~!@#$%*&()_—+=|:;"'`<>,.?\\/\\\s)]{1,30}$/;
 export const EMAIL_VALIDATION = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
+export const MIN_PASSWORD_LENGTH = 8;
