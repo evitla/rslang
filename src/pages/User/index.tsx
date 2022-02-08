7f9d80b29a84adbc1900ebc4498f79e1fc5d65c2
evitla <s.dinmuhamed@gmail.com>
2022-02-08 13:17:05 +0600

    feat: user logout button

--- src/pages/User/index.tsx
+++ src/pages/User/index.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import useLogout from '../../hooks/useLogout';
 
-const User = () => <h2>User page</h2>;
+const User = () => {
+  const { handleLogout } = useLogout();
+
+  return (
+    <div>
+      <button onClick={handleLogout}>Выйти из аккаунта</button>
+      // TODO: 3 components
+      <ol>
+        <li>Прогресс изучения слов</li>
+        <li>Изученные слова</li>
+        <li>Статистика</li>
+      </ol>
+    </div>
+  );
+};
 
 export default User;
