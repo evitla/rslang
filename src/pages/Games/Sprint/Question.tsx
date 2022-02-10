159ffd8a18ae822a52130d05f514cb29c893fd8e
Dmitriy <bmd4work@gmail.com>
2022-02-10 21:54:27 +0600

    feat: implement set words to state logic

--- src/pages/Games/Sprint/Question.tsx
+++ src/pages/Games/Sprint/Question.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import useFetchWords from '../../../hooks/useFetchWords';
+import { setWords } from '../../../slices/sprint';
 import Rightindicator from './Rightindicator';
 
 export default function Question() {
+  const dispatch = useDispatch();
+  const { words, isSuccess } = useFetchWords(1, 2);
+
+  useEffect(() => {
+    if (isSuccess) dispatch(setWords(words));
+  }, [words, isSuccess]);
+
   return (
     <div>
       <Rightindicator rightAnswerToBonus={2} rightInTheRow={3}></Rightindicator>
