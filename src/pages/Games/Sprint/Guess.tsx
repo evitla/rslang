482e4376dd2be21abd26629550e8696bcbbf9287
Dmitriy <bmd4work@gmail.com>
2022-02-11 18:31:32 +0600

    fix: remove bugs with rerender

--- src/pages/Games/Sprint/Guess.tsx
+++ src/pages/Games/Sprint/Guess.tsx
@@ -1,11 +1,65 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import {
+  setCurrentWord,
+  setCurrentWordIndex,
+  setRightAnswer,
+} from '../../../slices/sprint';
 import { TStore } from '../../../store';
+import { getRandomIntExcludingExistingNumbers } from '../../../utils';
 
 export default function Guess() {
-  const { currentWord } = useSelector(
+  const { currentWord, words, currentWordIndex, score } = useSelector(
     (state: TStore) => state.sprintGameReducer
   );
+  const dispatch = useDispatch();
+  const [variant, setvariant] = useState('');
+  function fiftyfifty() {
+    const result = Math.random();
+    return result > 0.5 ? true : false;
+  }
+  const result = fiftyfifty();
 
-  return <div>{currentWord}</div>;
+  useEffect(() => {
+    if (result) {
+      const translate = words[currentWordIndex].wordTranslate;
+      setvariant(translate);
+    } else {
+      const randomIndex = getRandomIntExcludingExistingNumbers(
+        0,
+        words.length - 1,
+        currentWordIndex
+      );
+      const translate = words[randomIndex].wordTranslate;
+      setvariant(translate);
+    }
+  }, [currentWord, words]);
+  function buttonHandler(userAnswer: boolean, index: number) {
+    if (userAnswer === result) {
+      dispatch(setRightAnswer());
+      dispatch(setCurrentWord(words[index + 1].word));
+    } else dispatch(setCurrentWord(words[index + 1].word));
+    dispatch(setCurrentWordIndex(index + 1));
+  }
+
+  return (
+    <div>
+      <div>score: {score}</div>
+      <div>Загадываемое слово: {currentWord}</div>
+      <div>Возможный вариант: {variant}</div>
+      <button
+        onClick={() => buttonHandler(false, currentWordIndex)}
+        type="button"
+      >
+        Не Верно
+      </button>
+      <button
+        onClick={() => buttonHandler(true, currentWordIndex)}
+        type="button"
+      >
+        {' '}
+        верно
+      </button>
+    </div>
+  );
 }
