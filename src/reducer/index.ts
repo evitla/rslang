b77122fcfff7d3e8e9ebd65036ee5f6047269166
evitla <s.dinmuhamed@gmail.com>
2022-02-13 14:50:30 +0600

    refactor: add wordReducer

--- src/reducer/index.ts
+++ src/reducer/index.ts
@@ -1,9 +1,11 @@
 import { userReducer } from '../slices/user';
 import { sprintGameReducer } from '../slices/sprint';
+import { wordReducer } from '../slices/word';
 
 const reducer = {
   userReducer,
   sprintGameReducer,
+  wordReducer,
 };
 
 export default reducer;
