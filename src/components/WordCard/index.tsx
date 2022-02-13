86f42ab1c568616c71d2ace4f9156839edde065f
evitla <s.dinmuhamed@gmail.com>
2022-02-13 15:32:04 +0600

    feat: handle word difficulty

--- src/components/WordCard/index.tsx
+++ src/components/WordCard/index.tsx
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Card, CardContent, ImageContainer, WordInfo } from './style';
 import { playAudio } from '../../utils';
 import { TWord } from '../../types';
 import { FILES_URL } from '../../constants';
+import useSetWordToDifficult from '../../hooks/useSetWordDifficult';
+import useRemoveWordFromDifficult from '../../hooks/useRemoveWordFromDifficult';
 
-const WordCard = ({ word }: { word: TWord }) => {
+const WordCard = ({
+  word,
+  isAuthorized,
+  isDifficultGroup,
+  isDifficult,
+}: {
+  word: TWord;
+  isAuthorized: boolean;
+  isDifficultGroup: boolean;
+  isDifficult: boolean;
+}) => {
+  const [isDifficultWord, setIsDifficultWord] = useState(isDifficult);
+
+  const { handleSetWordToDifficult } = useSetWordToDifficult(
+    word.id,
+    setIsDifficultWord
+  );
+
+  const { handleRemoveWordFromDifficult } = useRemoveWordFromDifficult(
+    word.id,
+    setIsDifficultWord
+  );
+
   return (
     <Card>
       <ImageContainer bgImage={`${FILES_URL}/${word.image}`} />
-      <CardContent>
+      <CardContent isDifficult={!isDifficultGroup && isDifficultWord}>
         <h2>{word.word}</h2>
         <WordInfo>
           <span>{word.wordTranslate}</span>
@@ -28,6 +52,12 @@ const WordCard = ({ word }: { word: TWord }) => {
         <p dangerouslySetInnerHTML={{ __html: word.textExample }} />
         <p dangerouslySetInnerHTML={{ __html: word.textMeaningTranslate }} />
         <p dangerouslySetInnerHTML={{ __html: word.textExampleTranslate }} />
+        {isAuthorized && !isDifficultWord && (
+          <button onClick={handleSetWordToDifficult}>Сложное</button>
+        )}
+        {isAuthorized && isDifficultGroup && (
+          <button onClick={handleRemoveWordFromDifficult}>Не сложное</button>
+        )}
       </CardContent>
     </Card>
   );
