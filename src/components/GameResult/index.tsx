02866611e96cdb4ea49fa3364edb693684d9f51d
Ruslan <kupzhasarov14@gmail.com>
2022-02-13 20:58:02 +0600

    feat: add final score

--- src/components/GameResult/index.tsx
+++ src/components/GameResult/index.tsx
@@ -2,8 +2,31 @@ import React from 'react';
 import { TAnswer } from '../../types';
 
 const GameResult = ({ userAnswers }: { userAnswers: TAnswer[] }) => {
-  console.log('userAnswers', userAnswers);
-  return <h2>Results</h2>;
+  const answers = userAnswers.map((answer) => {
+    return (
+      <tr>
+        <td>Sound</td>
+        <td>{answer.question}</td>
+        <td>transcript</td>
+        <td>{answer.correctAnswer}</td>
+        <td>{answer.correct ? 'Right' : 'Not Right'}</td>
+      </tr>
+    );
+  });
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th></th>
+          <th></th>
+          <th></th>
+          <th></th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>{answers}</tbody>
+    </table>
+  );
 };
 
 export default GameResult;
