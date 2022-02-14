188319c401f2e849843af3e826b84211a11b8291
Dmitriy <bmd4work@gmail.com>
2022-02-14 19:33:01 +0600

    feat: implement get stats API

--- src/pages/Statistic/TotalSection.tsx
+++ src/pages/Statistic/TotalSection.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { StatsState } from '../../types';
+
+type Tprops = Pick<StatsState, 'learnedWords' | 'totalRightPercent'>;
 
 export default function TotalSection() {
   return <div>TotalSection</div>;
