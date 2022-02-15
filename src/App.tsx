fbaac4cfee344c31fa8ec4fda83ab66f3889ee43
evitla <s.dinmuhamed@gmail.com>
2022-02-15 14:32:19 +0600

    feat: use context for auth form open state

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, Routes } from 'react-router';
 import { ThemeProvider } from 'styled-components';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import { SetIsAuthFormOpenContext } from './hooks/useOpenAuthForm';
 import { Audiocall, Book, BookPages, Games, Home, Sprint, User } from './pages';
 
 import GlobalStyles from './styles/global';
 import { baseTheme } from './styles/theme';
 import { Wrapper } from './styles/wrapper';
 
-const App = () => (
-  <ThemeProvider theme={baseTheme}>
-    <Header />
-    <hr />
-    <main>
-      <Wrapper>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="book" element={<Book />}>
-            <Route path=":groupId/:pageId" element={<BookPages />} />
-          </Route>
-          <Route path="user" element={<User />} />
-          <Route path="games" element={<Games />}>
-            <Route path="audiocall" element={<Audiocall />} />
-            <Route path="sprint" element={<Sprint />} />
-          </Route>
-        </Routes>
-      </Wrapper>
-    </main>
-    <hr />
-    <Footer />
-    <GlobalStyles />
-  </ThemeProvider>
-);
+const App = () => {
+  const [isAuthFormOpen, setIsAuthFormOpen] = useState(false);
+
+  return (
+    <ThemeProvider theme={baseTheme}>
+      <SetIsAuthFormOpenContext.Provider
+        value={{ isAuthFormOpen, setIsAuthFormOpen }}
+      >
+        <Header />
+        <hr />
+        <main>
+          <Wrapper>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="book" element={<Book />}>
+                <Route path=":groupId/:pageId" element={<BookPages />} />
+              </Route>
+              <Route path="user" element={<User />} />
+              <Route path="games" element={<Games />}>
+                <Route path="audiocall" element={<Audiocall />} />
+                <Route path="sprint" element={<Sprint />} />
+              </Route>
+            </Routes>
+          </Wrapper>
+        </main>
+        <hr />
+        <Footer />
+      </SetIsAuthFormOpenContext.Provider>
+      <GlobalStyles />
+    </ThemeProvider>
+  );
+};
 
 export default App;
