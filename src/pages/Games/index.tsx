6fe9649158e022ced320a417ceddb3f038db31f0
Ruslan <kupzhasarov14@gmail.com>
2022-02-15 19:56:49 +0600

    feaT: add sprint img

--- src/pages/Games/index.tsx
+++ src/pages/Games/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import { GameWrapper } from './style';
 import audiocallSVG from '../../assets/images/audiocall.svg';
+import sprintSVG from '../../assets/images/sprint.svg';
 
 const Games = () => (
   <GameWrapper>
@@ -24,14 +25,16 @@ const Games = () => (
       </div>
       <div className="card">
         <figure className="card__thumb">
-          <img className="card__image" src={audiocallSVG} alt="" />
+          <img className="card__image" src={sprintSVG} alt="" />
           <figcaption className="card__caption">
-            <h2 className="card__title">Аудиовызов</h2>
+            <h2 className="card__title">Спринт</h2>
             <p className="card__snippet">
-              Тренировка Аудиовызов развивает словарный запас. Вы должны выбрать
-              перевод услышанного слова.
+              В мини-игре "Спринт" от вас требуется выбрать - верен ли перевод
+              слова на русский язык.
             </p>
-            <Link to="sprint">Спринт</Link>
+            <Link to="sprint" className="card__button">
+              Играть
+            </Link>
           </figcaption>
         </figure>
       </div>
