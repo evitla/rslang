1f38daaa09f2cbcdd404fcc0e19b759e3f5c30b0
Ruslan <kupzhasarov14@gmail.com>
2022-02-18 19:30:19 +0600

    feat: add html for about team

--- src/pages/AboutTeam/index.tsx
+++ src/pages/AboutTeam/index.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
 
 const AboutTeam = () => {
-  return <h2>About Team кфя вмф</h2>;
+  return (
+    <div className="container">
+      <div className="card">
+        <div className="border">
+          <h2>evitla</h2>
+        </div>
+      </div>
+      <div className="card">
+        <div className="border">
+          <h2>excluz1v</h2>
+        </div>
+      </div>
+      <div className="card">
+        <div className="border">
+          <h2>hxnxikazuchi</h2>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AboutTeam;
