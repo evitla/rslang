490888d0823b2b6c9687db3ae01cf377cfa2262a
evitla <s.dinmuhamed@gmail.com>
2022-02-19 13:08:47 +0600

    refactor: add pageRangeDisplayed constant

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -11,6 +11,7 @@ export const TOTAL_GROUPS = 6;
 export const AUTH_TOTAL_GROUPS = TOTAL_GROUPS + 1;
 export const LOADING_BLOCKS_COUNT = 6;
 export const PAGES_AT_GROUP = 30;
+export const PAGE_RANGE_DISPLAYED = 5;
 export const ROUND_TIME = 60;
 export const POINTS = 20;
 export const BONUS_POINTS = 20;
