3514f222d7fcb534d79c46fd8f14d2c2432ef471
Ruslan <kupzhasarov14@gmail.com>
2022-02-19 22:53:58 +0600

    feat: add circle class

--- src/components/Loader/style.ts
+++ src/components/Loader/style.ts
@@ -7,4 +7,15 @@ export const StyledLoading = styled.div`
   left: 50%;
   top: 50%;
   transform: translate(-50%, -50%);
+
+  .circle {
+    width: 20px;
+    height: 20px;
+    position: absolute;
+    border-radius: 50%;
+    background-color: #fff;
+    left: 15%;
+    transform-origin: 50%;
+    animation: circle 0.5s alternate infinite ease;
+  }
 `;
