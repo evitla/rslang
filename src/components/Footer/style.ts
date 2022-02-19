a8329f84a9fd5ec7158eeeea45f41f1f8fb654da
Ruslan <kupzhasarov14@gmail.com>
2022-02-19 20:47:56 +0600

    feat: add height for footer

--- src/components/Footer/style.ts
+++ src/components/Footer/style.ts
@@ -3,6 +3,9 @@ import styled from 'styled-components';
 export const StyledFooter = styled.footer`
   padding: 10px 15px;
   margin-top: auto;
+  height: 60px;
+  display: flex;
+  align-items: center;
   ul {
     display: flex;
     justify-content: space-between;
